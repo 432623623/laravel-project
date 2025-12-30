4de1bc63944391b269272d4e9fead599977d629a
432623623 <patsong2015@gmail.com>
2025-12-30 15:16:52 +0900

    fix layout title and homepage text 5

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <div class="container">
+    <div class="container py-md-2">
       <div class="row align-items-center">
         <div class="col-lg-7 py-3 py-md-5">
           <h1 class="display-4">Laravel Blog App</h1>
