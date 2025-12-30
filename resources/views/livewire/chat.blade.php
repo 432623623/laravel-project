b71e5ecd47a7d2bf1e92de06967d7b8bd51904ed
432623623 <patsong2015@gmail.com>
2025-12-30 20:46:01 +0900

    layout

--- resources/views/livewire/chat.blade.php
+++ resources/views/livewire/chat.blade.php
@@ -1,6 +1,12 @@
 <div x-data="{isOpen: false}">
-    <span x-on:click="isOpen = true; document.querySelector('.chat-field').focus()" class="text-white mr-2 header-chat-icon" title="Chat" data-toggle="tooltip" data-placement="bottom"><i class="fas fa-comment"></i></span>
-    <div id="chat-wrapper" data-username="{{auth()->user()->username}}" data-avatar="{{auth()->user()->avatar}}" x-bind:class="isOpen ? 'chat--visible' : ''" class="chat-wrapper chat-wrapper--ready shadow border-top border-left border-right">
+    <span x-on:click="isOpen = true; document.querySelector('.chat-field').focus()" 
+      class="text-white mr-2 header-chat-icon" 
+      title="Chat" data-toggle="tooltip" 
+      data-placement="bottom"><i class="fas fa-comment"></i></span>
+    <div id="chat-wrapper" data-username="{{auth()->user()->username}}" 
+      data-avatar="{{auth()->user()->avatar}}" 
+      x-bind:class="isOpen ? 'chat--visible' : ''" 
+      class="chat-wrapper chat-wrapper--ready shadow border-top border-left border-right">
     <div class="chat-title-bar">Chat <span x-on:click="isOpen = false" class="chat-title-bar-close"><i class="fas fa-times-circle"></i></span></div>
     <div id="chat" class="chat-log">
         @if(count($chatLog) > 0)
