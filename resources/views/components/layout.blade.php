a0e58c6b67dededf86b6c322325f2d1381988697
432623623 <patsong2015@gmail.com>
2025-12-30 15:09:35 +0900

    fix layout title and homepage text 2

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,7 +8,7 @@
       @isset($doctitle)
       {{$doctitle}} | Laravel App
       @else
-      OurApp
+      Laravel App
       @endisset
     </title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
