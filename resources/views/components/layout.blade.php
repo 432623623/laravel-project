9a659c6095800c61460308a170cd424710d2658d
432623623 <patsong2015@gmail.com>
2025-12-30 15:10:39 +0900

    fix layout title and homepage text 3

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -22,7 +22,7 @@
   <body>
     <header class="header-bar mb-3">
       <div class="container d-flex flex-column flex-md-row align-items-center p-3">
-        <h4 class="my-0 mr-md-auto font-weight-normal"><a wire:navigate href="/" class="text-white">OurApp</a></h4>
+        <h4 class="my-0 mr-md-auto font-weight-normal"><a wire:navigate href="/" class="text-white">Laravel App</a></h4>
         @auth
             <div class="flex-row my-3 my-md-0">
               @persist('headerdynamic')
