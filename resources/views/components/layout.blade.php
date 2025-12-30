3f745855732676c95e66ff188ffc0d805f049dff
432623623 <patsong2015@gmail.com>
2025-12-30 15:07:46 +0900

    fix layout title and homepage text

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>
       @isset($doctitle)
-      {{$doctitle}} | OurApp
+      {{$doctitle}} | Laravel App
       @else
       OurApp
       @endisset
