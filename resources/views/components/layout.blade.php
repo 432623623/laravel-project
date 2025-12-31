016db4c515493a4a472233973649804bd90460c9
432623623 <patsong2015@gmail.com>
2025-12-31 20:18:46 +0900

    fonts

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -17,8 +17,7 @@
     <script defer src="https://use.fontawesome.com/releases/v5.5.0/js/all.js" integrity="sha384-GqVMZRt5Gn7tB9D9q7ONtcp4gtHIUEW/yG7h98J7IpE3kpi+srfFyyB/04OV6pG0" crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
-    @vite(['resources/css/app.css'])
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@700&family=Roboto:wght@400;500&display=swap" rel="stylesheet">    @vite(['resources/css/app.css'])
     @vite(['resources/js/app.js'])
     <livewire:styles />
   </head>
