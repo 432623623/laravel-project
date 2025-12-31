785038b8ec4ec05c32bdfd688f92f2e68c352ae4
432623623 <patsong2015@gmail.com>
2025-12-31 09:45:30 +0900

    comment out echo and pusher

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,7 +8,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allow your team to quickly build robust real-time web applications.
  */
-
+/*
 import Echo from 'laravel-echo';
 
 import Pusher from 'pusher-js';
@@ -25,4 +25,4 @@ window.Echo = new Echo({
     enabledTransports: ["ws", "wss"],
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
 });
-
+*/
