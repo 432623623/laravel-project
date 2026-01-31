625b5cadeec9d20844d9e59a6d0a673253d4e8f8
432623623 <patsong2015@gmail.com>
2026-02-01 08:42:14 +0900

    trix post thumbnail 10

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -81,7 +81,7 @@ class PostController extends Controller
 
         $thumbImage = $manager->read($file->getRealPath());
         $thumbPath = storage_path("app/public/trix-images/thumb/$filename");
-        $thumbImage->scale(width:300, height:200)->save($thumbPath, quality: 85);  
+        $thumbImage->scale(width:300)->save($thumbPath, quality: 85);  
 
         return response()->json([
             'url'=>asset("storage/trix-images/thumb/$filename"),
